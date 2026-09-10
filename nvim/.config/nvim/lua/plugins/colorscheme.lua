277ecc7bb632c5e446e0fcfcc18a7e475dd00c6a
g-machado-uu <g.machado@ulster.ac.uk>
2026-09-10 16:22:03 +0100

    added nvim, vim, and browser flags

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    {
+      "catppuccin",
+      opts = {
+        transparent_background = true,
+        float = {
+          transparent = true,
+          solid = true,
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-nvim",
+    },
+  },
+}
